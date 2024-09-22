fbc8f5b8f55b7256dc92de235d99e1010d16e611
Jose Rojas <jgra11.2010@gmail.com>
2024-09-22 17:50:31 -0400

    Added custom color to the tailwindcss config file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				darkGray: '#3A3A3A',
+				banner1: '#E5A41B'
+			}
+		}
 	},
 	plugins: []
 }
